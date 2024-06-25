468a90126cece575f102edd3dbaad97070951ed4
Dongmin <pdm001125@naver.com>
2024-06-26 03:43:03 +0900

    feature #1: version control

--- feature/src/main/java/com/eoyeongbooyeong/feature/MainActivity.kt
+++ feature/src/main/java/com/eoyeongbooyeong/feature/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             BooyoungeeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Main",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
