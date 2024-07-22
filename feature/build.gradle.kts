8b39fc8c7e39f85be42e1d32fcfbc414a2016414
kangyuri1114 <yuri3334@gmail.com>
2024-07-22 12:58:24 +0900

    feature #3: META-INF 에러 해결

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -45,6 +45,8 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/gradle/incremental.annotation.processors"
+
         }
     }
 }
