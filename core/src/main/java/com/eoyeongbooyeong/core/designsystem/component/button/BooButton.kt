7b8b3b1955e268fc5e0239fc3e94755e1624486b
Dongmin <pdm001125@naver.com>
2024-07-23 01:11:19 +0900

    feature #7: feat gradient button

--- core/src/main/java/com/eoyeongbooyeong/core/designsystem/component/button/BooButton.kt
+++ core/src/main/java/com/eoyeongbooyeong/core/designsystem/component/button/BooButton.kt
@@ -2,6 +2,8 @@ package com.eoyeongbooyeong.core.designsystem.component.button
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -9,8 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eoyeongbooyeong.core.designsystem.theme.Blue400
 import com.eoyeongbooyeong.core.designsystem.theme.White
@@ -20,8 +24,10 @@ import com.eoyeongbooyeong.core.extension.noRippleClickable
 fun BooBaseButton(
     text: String,
     modifier: Modifier = Modifier,
+    paddingValues: PaddingValues = PaddingValues(0.dp),
     enabled: Boolean = true,
     backgroundColor: Color = Blue400,
+    backgroundGradient: Brush? = null,
     textColor: Color = White,
     textStyle: TextStyle = TextStyle.Default,
     onClick: () -> Unit = {},
@@ -34,7 +40,12 @@ fun BooBaseButton(
                 else this
             }
             .clip(RoundedCornerShape(10.dp))
-            .background(backgroundColor)
+            .run {
+                if (backgroundGradient != null) this.background(backgroundGradient)
+                else this.background(backgroundColor)
+            }
+            .padding(paddingValues)
+
     ) {
         Text(
             text = text,
@@ -48,17 +59,21 @@ fun BooBaseButton(
 fun BooLargeButton(
     text: String,
     modifier: Modifier = Modifier,
+    paddingValues: PaddingValues = PaddingValues(horizontal = 22.dp, vertical = 17.dp),
     enabled: Boolean = true,
     backgroundColor: Color = Blue400,
+    backgroundGradient: Brush? = null,
     textColor: Color = White,
     textStyle: TextStyle = TextStyle.Default,
     onClick: () -> Unit = {},
 ) {
     BooBaseButton(
         text = text,
-        modifier = modifier.padding(horizontal = 22.dp, vertical = 17.dp),
+        modifier = modifier,
         enabled = enabled,
+        paddingValues = paddingValues,
         backgroundColor = backgroundColor,
+        backgroundGradient = backgroundGradient,
         textColor = textColor,
         textStyle = textStyle,
         onClick = onClick
@@ -69,17 +84,29 @@ fun BooLargeButton(
 fun BooMiniButton(
     text: String,
     modifier: Modifier = Modifier,
+    paddingValues: PaddingValues = PaddingValues(horizontal = 22.dp, vertical = 17.dp),
     backgroundColor: Color = Blue400,
     textColor: Color = White,
     textStyle: TextStyle = TextStyle.Default,
-    onClick: () -> Unit = {}
+    onClick: () -> Unit = {},
 ) {
     BooBaseButton(
         text = text,
         onClick = onClick,
-        modifier = modifier.padding(horizontal = 16.dp, vertical = 14.5.dp),
+        modifier = modifier,
+        paddingValues = paddingValues,
         backgroundColor = backgroundColor,
         textColor = textColor,
         textStyle = textStyle
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BooButtonsPreview() {
+    Column {
+        BooBaseButton(text = "Base Button")
+        BooMiniButton(text = "Mini Button")
+        BooLargeButton(text = "Large Button")
+    }
 }
