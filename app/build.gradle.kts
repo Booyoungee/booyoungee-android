32ba9823d2fc542c065bc753ce3859e831934f2f
Dongmin <pdm001125@naver.com>
2024-07-22 22:47:18 +0900

    feature #7: add version controller

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 
 android {
     namespace = "com.eoyeongbooyeong.booyoungee"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.eoyeongbooyeong.booyoungee"
-        minSdk = 28
+        minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
