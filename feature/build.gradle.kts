7e9afcc3f8a27516bb9c468d5e97409a68ee390f
kangyuri1114 <yuri3334@gmail.com>
2024-07-31 20:58:44 +0900

    feat #11: 현재 위치를 불러오기 위한 google.play.services.location dependency 추가

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -75,6 +75,9 @@ dependencies {
     implementation(libs.kakao.user)
     implementation(libs.kakao.map)
 
+    //google play service
+    implementation(libs.google.play.services.location)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
