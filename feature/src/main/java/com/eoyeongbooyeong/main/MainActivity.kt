45f86fd431b69d9e80773c0da193077fa20a8606
kangyuri1114 <yuri3334@gmail.com>
2024-07-31 18:47:00 +0900

    feat #11: set KakaoMap View

--- feature/src/main/java/com/eoyeongbooyeong/main/MainActivity.kt
+++ feature/src/main/java/com/eoyeongbooyeong/main/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.eoyeongbooyeong.home.HomeScreen
 import com.eoyeongbooyeong.main.ui.theme.BooyoungeeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,12 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BooyoungeeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Main",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                HomeScreen()
             }
         }
     }
