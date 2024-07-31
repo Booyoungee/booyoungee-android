d5a673ae0567420b3696014a2bacbcbf6ef48512
kangyuri1114 <yuri3334@gmail.com>
2024-07-31 18:47:00 +0900

    feat #11: set KakaoMap View (rebase)

--- feature/src/main/java/com/eoyeongbooyeong/home/HomeScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/home/HomeScreen.kt
@@ -1,9 +1,65 @@
 package com.eoyeongbooyeong.home
 
-import android.util.Log
+import android.content.Context
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
 
 @Composable
 internal fun HomeScreen() {
-    Log.d("HomeScreen", "HomeScreen")
+
+    val context = LocalContext.current
+    val mapView = rememberMapView(context = context)
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding(),
+    ) { paddingValue ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValue)
+        ) {
+            AndroidView(
+                factory = {
+                    mapView
+                },
+            )
+        }
+    }
+}
+
+@Composable
+fun rememberMapView(
+    context: Context,
+): MapView {
+    val mapView = remember {
+        MapView(context).also { mapView ->
+            mapView.start(
+                object : MapLifeCycleCallback() {
+                    override fun onMapDestroy() = Unit
+                    override fun onMapError(e: Exception?) = Unit
+                    override fun onMapResumed() = Unit
+                },
+
+                object : KakaoMapReadyCallback() {
+                    override fun onMapReady(map: KakaoMap) = Unit
+                }
+            )
+        }
+    }
+
+    return mapView
 }
