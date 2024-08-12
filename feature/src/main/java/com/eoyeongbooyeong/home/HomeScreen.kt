a2df9c605492e2ad8462ea34219d48336b2e93dd
kangyuri1114 <yuri3334@gmail.com>
2024-08-12 23:27:26 +0900

    feat #11: HomeScreen BooSearchTextField 추가

--- feature/src/main/java/com/eoyeongbooyeong/home/HomeScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/home/HomeScreen.kt
@@ -1,23 +1,31 @@
 package com.eoyeongbooyeong.home
 
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.eoyeongbooyeong.core.designsystem.component.textfield.BooSearchTextField
+import com.eoyeongbooyeong.core.designsystem.theme.White
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
 
 @Composable
-internal fun HomeScreen() {
+internal fun HomeScreen(
+    searchText: String?,
+) {
 
     val context = LocalContext.current
     val mapView = rememberMapView(context = context)
@@ -25,18 +33,28 @@ internal fun HomeScreen() {
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
-            .systemBarsPadding(),
+            .systemBarsPadding()
     ) { paddingValue ->
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValue)
+                .background(color = White)
         ) {
-            AndroidView(
-                factory = {
-                    mapView
-                },
+            BooSearchTextField(
+                text = searchText ?: "",
+                modifier = Modifier.padding(horizontal = 24.dp, vertical = 12.dp)
             )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValue)
+            ) {
+                AndroidView(
+                    factory = {
+                        mapView
+                    },
+                )
+            }
         }
     }
 }
