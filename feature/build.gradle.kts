04a841414ac77a3a5d8a6eecec8d1434753819eb
kangyuri1114 <yuri3334@gmail.com>
2024-08-20 02:45:51 +0900

    feat #13: coil-svg 의존성 추가

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -71,6 +71,10 @@ dependencies {
     //Timber
     implementation(libs.timber)
 
+    // Coil
+    implementation(libs.compose.coil)
+    implementation(libs.coil.svg)
+
     //Kakao SDK
     implementation(libs.kakao.user)
     implementation(libs.kakao.map)
