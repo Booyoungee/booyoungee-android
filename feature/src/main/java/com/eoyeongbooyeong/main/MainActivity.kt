738b9cf011dcbd2487c37b5f0caea6b3335bac06
kangyuri1114 <yuri3334@gmail.com>
2024-08-22 23:20:05 +0900

    feat #13: main 액티비티 파일 정리

--- feature/src/main/java/com/eoyeongbooyeong/main/MainActivity.kt
+++ feature/src/main/java/com/eoyeongbooyeong/main/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.eoyeongbooyeong.core.designsystem.theme.BooTheme
 import com.eoyeongbooyeong.home.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,24 +18,5 @@ class MainActivity : ComponentActivity() {
                 HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BooTheme {
-        Greeting("Android")
     }
 }
