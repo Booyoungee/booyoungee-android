74b5414d0288400927ae5b3533eba7cceeffbad9
kangyuri1114 <yuri3334@gmail.com>
2024-08-25 22:26:28 +0900

    feat #5: 불필요한 코드 삭제

--- feature/src/main/java/com/eoyeongbooyeong/main/MainActivity.kt
+++ feature/src/main/java/com/eoyeongbooyeong/main/MainActivity.kt
@@ -4,15 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.eoyeongbooyeong.core.designsystem.theme.BooTheme
-import com.eoyeongbooyeong.main.ui.theme.BooyoungeeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
