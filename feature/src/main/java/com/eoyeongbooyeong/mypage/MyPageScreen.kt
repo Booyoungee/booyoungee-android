20f0d7e111495ea66b7478b1797437f2303da2d5
kangyuri1114 <yuri3334@gmail.com>
2024-08-25 22:07:43 +0900

    feat #5: 각 스크린 log 추가

--- feature/src/main/java/com/eoyeongbooyeong/mypage/MyPageScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/mypage/MyPageScreen.kt
@@ -1,8 +1,9 @@
 package com.eoyeongbooyeong.mypage
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 
 @Composable
 internal fun MyPageScreen() {
-
+    Log.d("MyPageScreen", "MyPageScreen")
 }
