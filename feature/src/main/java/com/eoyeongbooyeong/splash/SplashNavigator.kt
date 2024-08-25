5dff70abdc9c842109a861708427ab0766154ec4
kangyuri1114 <yuri3334@gmail.com>
2024-08-25 18:18:47 +0900

    feat #5: splash 패키지, navigator 추가

---
+++ feature/src/main/java/com/eoyeongbooyeong/splash/SplashNavigator.kt
@@ -0,0 +1,7 @@
+package com.eoyeongbooyeong.splash
+
+import com.eoyeongbooyeong.navigation.MainTabRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object Splash : MainTabRoute
