e5ceaa17d77c29e0fb370e5b4c9767ddb098fcdf
kangyuri1114 <yuri3334@gmail.com>
2024-08-26 03:20:09 +0900

    feat #16: searchbar 추가

--- feature/src/main/java/com/eoyeongbooyeong/search/screen/NoResultScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/search/screen/NoResultScreen.kt
@@ -1,7 +1,16 @@
 package com.eoyeongbooyeong.search.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,48 +20,110 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.eoyeongbooyeong.core.designsystem.component.textfield.BooSearchTextField
 import com.eoyeongbooyeong.core.designsystem.theme.Black
 import com.eoyeongbooyeong.core.designsystem.theme.BooTheme
 import com.eoyeongbooyeong.core.designsystem.theme.Gray400
+import com.eoyeongbooyeong.core.designsystem.theme.White
 import com.eoyeongbooyeong.feature.R
 
 @Composable
-fun NoResultScreen(searchKeyWord: String = "영화") {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+fun NoResultScreen(
+    modifier: Modifier,
+    searchKeyWord: String = "영화",
+    onBackClick: () -> Unit,
+    onQueryChange: (String) -> Unit,
+    onActiveChange: (Boolean) -> Unit,
+    query: String,
+    active: Boolean,
+) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Spacer(modifier = Modifier.padding(12.dp))
+
+        Box(
+            modifier = modifier.fillMaxWidth().padding(horizontal = 24.dp),
+            contentAlignment = Alignment.TopCenter,
         ) {
-            Image(
-                painter = painterResource(id = com.eoyeongbooyeong.core.R.drawable.ic_alert_circle),
-                contentDescription = "no result icon",
+            Row(
                 modifier =
-                Modifier
-                    .size(100.dp)
-                    .padding(bottom = 12.dp),
-            )
-
-            Text(
-                text = stringResource(R.string.noSearchResult, searchKeyWord),
-                style = BooTheme.typography.body1,
-                color = Black,
-                modifier = Modifier.padding(bottom = 12.dp).align(Alignment.CenterHorizontally),
-                textAlign = TextAlign.Center,
-            )
-
-            Text(
-                text = stringResource(R.string.checkSearchResult),
-                style = BooTheme.typography.body4,
-                color = Gray400,
-            )
+                    modifier
+                        .fillMaxWidth(),
+            ) {
+                Image(
+                    painter = painterResource(id = com.eoyeongbooyeong.core.R.drawable.ic_left),
+                    contentDescription = "back button",
+                    modifier =
+                        Modifier
+                            .padding(6.dp)
+                            .clickable(onClick = onBackClick)
+                            .size(24.dp)
+                            .align(Alignment.CenterVertically),
+                )
+                BooSearchTextField(
+                    text = query,
+                    onValueChange = onQueryChange,
+                    isActive = active,
+                    onClick = { onActiveChange(true) },
+                    modifier = Modifier.weight(1f),
+                )
+            }
         }
+
+        Spacer(modifier = Modifier.weight(5f))
+
+        Box(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .background(White),
+            contentAlignment = Alignment.Center,
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painterResource(id = com.eoyeongbooyeong.core.R.drawable.ic_alert_circle),
+                    contentDescription = "no result icon",
+                    modifier =
+                        Modifier
+                            .size(100.dp)
+                            .padding(bottom = 12.dp),
+                )
+
+                Text(
+                    text = stringResource(R.string.noSearchResult, searchKeyWord),
+                    style = BooTheme.typography.body1,
+                    color = Black,
+                    modifier =
+                        Modifier
+                            .padding(bottom = 12.dp)
+                            .align(Alignment.CenterHorizontally),
+                    textAlign = TextAlign.Center,
+                )
+
+                Text(
+                    text = stringResource(R.string.checkSearchResult),
+                    style = BooTheme.typography.body4,
+                    color = Gray400,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(5f))
     }
 }
 
 @Composable
 @Preview
 fun NoResultScreenPreview() {
-    NoResultScreen()
+    BooTheme {
+        NoResultScreen(
+            modifier = Modifier.background(White),
+            searchKeyWord = "영화",
+            onBackClick = {},
+            onQueryChange = {},
+            onActiveChange = {},
+            query = "",
+            active = false,
+        )
+    }
 }
