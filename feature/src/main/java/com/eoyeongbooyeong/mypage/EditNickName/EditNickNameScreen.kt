8745237f97a1e72a440c49a0e34d05c1d5babe23
Dongmin <pdm001125@naver.com>
2024-09-04 02:34:03 +0900

    feature #26: packaged

--- feature/src/main/java/com/eoyeongbooyeong/mypage/EditNickName/EditNickNameScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/mypage/EditNickName/EditNickNameScreen.kt
@@ -1,4 +1,4 @@
-package com.eoyeongbooyeong.mypage
+package com.eoyeongbooyeong.mypage.EditNickName
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
