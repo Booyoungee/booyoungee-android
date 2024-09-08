acfa3f7d3b1e6a361b8d7b80ede6613d02baeea3
kangyuri1114 <yuri3334@gmail.com>
2024-09-09 01:07:18 +0900

    feat #32: KakaoMapViewModel 세팅 및 장소 상세보기 API 세팅

---
+++ feature/src/main/java/com/eoyeongbooyeong/places/map/KakaoMapViewModel.kt
@@ -0,0 +1,45 @@
+package com.eoyeongbooyeong.places.map
+
+import androidx.lifecycle.ViewModel
+import com.eoyeongbooyeong.domain.repository.PlaceRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class KakaoMapViewModel
+    @Inject
+    constructor(
+        private val placeRepository: PlaceRepository,
+    ) : ViewModel() {
+        private val _state = MutableStateFlow(KakaoMapState())
+        val state: StateFlow<KakaoMapState>
+            get() = _state.asStateFlow()
+
+        private val _sideEffects = MutableSharedFlow<KakaoMapSideEffect>()
+        val sideEffects: SharedFlow<KakaoMapSideEffect>
+            get() = _sideEffects.asSharedFlow()
+
+        suspend fun getPlaceDetailsInfo(
+            placeId: Int,
+            placeType: String,
+        ) {
+            placeRepository
+                .getPlaceDetails(placeId, placeType)
+                .onSuccess {
+                    _state.value =
+                        _state.value.copy(
+                            placeDetailsInfo = it,
+                        )
+                    Timber.tag("PlaceDetailsViewModel").d(it.toString())
+                }.onFailure {
+                    _sideEffects.emit(KakaoMapSideEffect.ShowToast(it.message.toString()))
+                }
+        }
+    }
