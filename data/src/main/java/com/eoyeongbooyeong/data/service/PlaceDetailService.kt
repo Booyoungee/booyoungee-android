50323059dcbee93b6c9f16d0b407f2342e3d19ad
kangyuri1114 <yuri3334@gmail.com>
2024-09-08 22:02:12 +0900

    feat #32: PlaceDetailService, PlaceDetailDto 추가

---
+++ data/src/main/java/com/eoyeongbooyeong/data/service/PlaceDetailService.kt
@@ -0,0 +1,14 @@
+package com.eoyeongbooyeong.data.service
+
+import com.eoyeongbooyeong.data.dto.response.BaseResponse
+import com.eoyeongbooyeong.data.dto.response.PlaceDetailsDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PlaceDetailService {
+    @GET("api/vq/place/details")
+    suspend fun getPlaceDetails(
+        @Query("placeId") placeId: Int,
+        @Query("placeType") placeType: String,
+    ): BaseResponse<PlaceDetailsDto>
+}
