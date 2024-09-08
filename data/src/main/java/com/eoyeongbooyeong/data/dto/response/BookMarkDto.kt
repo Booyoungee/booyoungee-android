c375853d3daaac295d6c3bf596bb18d43f63c1f8
kangyuri1114 <yuri3334@gmail.com>
2024-09-09 01:24:05 +0900

    feat #32:  bookmark 관련 service함수, dto 추가

---
+++ data/src/main/java/com/eoyeongbooyeong/data/dto/response/BookMarkDto.kt
@@ -0,0 +1,10 @@
+package com.eoyeongbooyeong.data.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BookMarkDto(
+    @SerialName("bookmarkId")
+    val bookmarkId: String,
+)
--- data/src/main/java/com/eoyeongbooyeong/data/service/PlaceService.kt
+++ data/src/main/java/com/eoyeongbooyeong/data/service/PlaceService.kt
@@ -1,8 +1,11 @@
 package com.eoyeongbooyeong.data.service
 
 import com.eoyeongbooyeong.data.dto.response.BaseResponse
+import com.eoyeongbooyeong.data.dto.response.BookMarkDto
 import com.eoyeongbooyeong.data.dto.response.PlaceDetailsDto
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface PlaceService {
@@ -11,4 +14,15 @@ interface PlaceService {
         @Query("placeId") placeId: Int,
         @Query("placeType") placeType: String,
     ): BaseResponse<PlaceDetailsDto>
+
+    @POST("api/v1/bookmark")
+    suspend fun postBookMark(
+        @Query("placeId") placeId: Int,
+        @Query("type") placeType: String,
+    ): BaseResponse<BookMarkDto>
+
+    @DELETE("api/v1/bookmark")
+    suspend fun deleteBookMark(
+        @Query("bookMarkId") bookMarkId: Int,
+    ): BaseResponse<BookMarkDto>
 }
