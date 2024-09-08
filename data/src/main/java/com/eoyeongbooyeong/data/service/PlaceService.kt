360ce033e79a2747cbe646c08581d688b5ff7de1
kangyuri1114 <yuri3334@gmail.com>
2024-09-09 01:01:02 +0900

    feat #32: 서비스 주소, 명 변경

--- data/src/main/java/com/eoyeongbooyeong/data/service/PlaceService.kt
+++ data/src/main/java/com/eoyeongbooyeong/data/service/PlaceService.kt
@@ -5,8 +5,8 @@ import com.eoyeongbooyeong.data.dto.response.PlaceDetailsDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface PlaceDetailsService {
-    @GET("api/vq/place/details")
+interface PlaceService {
+    @GET("api/v1/place/details")
     suspend fun getPlaceDetails(
         @Query("placeId") placeId: Int,
         @Query("placeType") placeType: String,
