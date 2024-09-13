ad94b6a63d9c4c89ea5ee4f32827aa08b7d1dfc5
Dongmin <pdm001125@naver.com>
2024-09-13 21:57:58 +0900

    feature #33: feat stamp empty view

--- feature/src/main/java/com/eoyeongbooyeong/stamp/StampScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/stamp/StampScreen.kt
@@ -1,10 +1,54 @@
 package com.eoyeongbooyeong.stamp
 
-import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.eoyeongbooyeong.core.designsystem.theme.Black
+import com.eoyeongbooyeong.core.designsystem.theme.BooTheme
+import com.eoyeongbooyeong.core.designsystem.theme.White
 
 @Composable
 internal fun StampScreen() {
-    Log.d("StampScreen", "StampScreen")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(White),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = com.eoyeongbooyeong.core.R.drawable.img_splash_logo),
+                contentDescription = "Stamp",
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = "아직 여러분들과 만날 준비중이에요!\n조금만 기다려주세요!",
+                style = BooTheme.typography.head1,
+                color = Black,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun PreviewStampScreen() {
+    BooTheme {
+        StampScreen()
+    }
 }
