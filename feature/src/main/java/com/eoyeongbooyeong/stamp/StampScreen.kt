3d722b6fa4c0fe284868ceb09b02d7f529d8e83a
Dongmin <pdm001125@naver.com>
2024-09-18 19:52:53 +0900

    feature #45: set background

--- feature/src/main/java/com/eoyeongbooyeong/stamp/StampScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/stamp/StampScreen.kt
@@ -1,13 +1,20 @@
 package com.eoyeongbooyeong.stamp
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
@@ -15,6 +22,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eoyeongbooyeong.core.designsystem.component.topbar.BooTextTopAppBar
@@ -46,7 +56,7 @@ private fun StampScreen(
             text = "스탬프",
         )
 
-        val tabData = listOf("1번", "2번")
+        val tabData = listOf("만날 수 있는 부영이", "햄깅이22 님의 부영이")
         val pagerState = rememberPagerState(initialPage = 0) { tabData.size }
         val coroutineScope = rememberCoroutineScope()
 
@@ -84,10 +94,48 @@ private fun StampScreen(
             modifier = Modifier.background(White)
         ) { index ->
             Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .paint(
+                        painter = painterResource(com.eoyeongbooyeong.core.R.drawable.img_stamp_background),
+                        contentScale = ContentScale.FillBounds
+                    )
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Card(
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .fillMaxWidth(),
+                    colors = CardDefaults.cardColors().copy(
+                        containerColor = White
+                    )
+                ) {
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        text = "내가 모은 부영이",
+                        style = BooTheme.typography.body3,
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "8",
+                            style = BooTheme.typography.head1,
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            text = "마리",
+                            style = BooTheme.typography.body2,
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                }
                 Text(if (index == 0) "1번 스탬프" else "2번 스탬프")
             }
         }
