a3eb82815a757931103b18c07211e51fd17b14d5
Dongmin <pdm001125@naver.com>
2024-09-18 14:16:00 +0900

    feature #45: feat dummy ui

--- feature/src/main/java/com/eoyeongbooyeong/stamp/StampScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/stamp/StampScreen.kt
@@ -1,46 +1,79 @@
 package com.eoyeongbooyeong.stamp
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.eoyeongbooyeong.core.designsystem.component.topbar.BooTextTopAppBar
 import com.eoyeongbooyeong.core.designsystem.theme.Black
 import com.eoyeongbooyeong.core.designsystem.theme.BooTheme
+import com.eoyeongbooyeong.core.designsystem.theme.Gray400
 import com.eoyeongbooyeong.core.designsystem.theme.White
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun StampScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(White),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier.fillMaxSize(),
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+        BooTextTopAppBar(
+            text = "스탬프",
+        )
+
+        val tabData = listOf("1번", "2번")
+        val pagerState = rememberPagerState(initialPage = 0) { tabData.size }
+        val coroutineScope = rememberCoroutineScope()
+
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            modifier = Modifier,
+            containerColor = White,
         ) {
-            Image(
-                painter = painterResource(id = com.eoyeongbooyeong.core.R.drawable.img_splash_logo),
-                contentDescription = "Stamp",
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-            Text(
-                text = "아직 여러분들과 만날 준비중이에요!\n조금만 기다려주세요!",
-                style = BooTheme.typography.head1,
-                color = Black,
-                textAlign = TextAlign.Center
-            )
+            tabData.forEachIndexed { index, text ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = {
+                        Text(
+                            text = text,
+                            style = BooTheme.typography.caption1,
+                            color = if (pagerState.currentPage == index) {
+                                Black
+                            } else {
+                                Gray400
+                            }
+                        )
+                    },
+                    modifier = Modifier.background(White)
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.background(White)
+        ) { index ->
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(if (index == 0) "1번 스탬프" else "2번 스탬프")
+            }
         }
     }
 }
