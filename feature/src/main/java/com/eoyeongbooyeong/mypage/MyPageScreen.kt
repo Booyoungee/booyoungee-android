f23179d1d1a8e2bcc35ff4979bea6fac78846d4a
Dongmin <pdm001125@naver.com>
2024-09-24 14:46:31 +0900

    update version 1.0.1 -> 1.1.0

--- feature/src/main/java/com/eoyeongbooyeong/mypage/MyPageScreen.kt
+++ feature/src/main/java/com/eoyeongbooyeong/mypage/MyPageScreen.kt
@@ -227,7 +227,7 @@ internal fun MyPageScreen(
                     style = BooTheme.typography.caption2
                 )
                 Text(
-                    text = "1.0.1",
+                    text = "1.1.0",
                     color = Gray400,
                     style = BooTheme.typography.caption2
                 )
